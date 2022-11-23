b04bc2e362ca634ebd942158d8d48d7edf9b5aa8
MallowHerman <hgoncalves2019@gmail.com>
2022-11-23 21:52:24 -0100

    templates layout and home

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'Herman Gonçalves',
+        'title': 'Blog Post 1',
+        'content': 'blablabla',
+        'date_posted': '23/11/2022'
+    },
+    {
+        'author': 'Herman Gonçalves',
+        'title': 'Blog Post 1',
+        'content': 'blablabla',
+        'date_posted': '23/11/2022'
+    },
+    {
+        'author': 'Herman Gonçalves',
+        'title': 'Blog Post 1',
+        'content': 'blablabla',
+        'date_posted': '23/11/2022'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+	return render(request, 'blog/home.html', {'posts':posts, 'title': 'Home'})
 
 def about(request):
     return render(request, 'blog/about.html')
