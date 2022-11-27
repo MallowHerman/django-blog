ba052c8f814eb4545002951ad5d4143ef28f3aaa
Herman <hgoncalves2019@gmail.com>
2022-11-27 16:37:24 -0100

    Login and Logout incompleted

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home' # Redirecionar para página principal depois de sucesso de login
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
